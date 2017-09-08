dbb5d41df8f36597c2b6232e657b078e7df82f85
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2017-09-08 23:31:51 +0530

    updates

--- BTVatHLT/ProduceNtuple/launchNtupleFromAOD2017_forDeepCSV.py
+++ BTVatHLT/ProduceNtuple/launchNtupleFromAOD2017_forDeepCSV.py
@@ -919,7 +919,7 @@ def launchNtupleFromAOD2017(fileOutput,filesInput,maxevents):
     f.Write()
     f.Close()
 
-maxevents=200
-fileOutput = 'tree.root'
-filesInput = 'outputFULL.root'
-launchNtupleFromAOD2017(fileOutput,filesInput,maxevents)
+#maxevents=200
+#fileOutput = 'tree.root'
+#filesInput = 'outputFULL.root'
+#launchNtupleFromAOD2017(fileOutput,filesInput,maxevents)
